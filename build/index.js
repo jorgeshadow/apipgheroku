f965f2af9b740e3a70975cfc168afa95cb3a336b
jorge luis sanchez ramirez <42231910+jorgeshadow@users.noreply.github.com>
2022-01-10 09:10:47 -0600

    asfasf
    asfsf

--- build/index.js
+++ build/index.js
@@ -13,7 +13,7 @@ class Server {
         this.onRoutes();
     }
     onConfig() {
-        this.app.set('port', process.env.PORT || 3000);
+        this.app.set('port', process.env.PORT || 8000);
         this.app.use(express_1.default.json());
         this.app.use(cors());
         this.app.use(express_1.default.urlencoded({ extended: false }));
