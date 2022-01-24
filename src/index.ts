10dbedd3f60975193401bf613d342b076167fcd7
jorge luis sanchez ramirez <42231910+jorgeshadow@users.noreply.github.com>
2022-01-24 16:56:45 -0600

    s

--- src/index.ts
+++ src/index.ts
@@ -100,6 +100,7 @@ from sucursales s                                               `
             res.json(err)
             });
     });  
+    
 
 app.listen(app.get("port"), () => {
   console.log(`Server on http://localhost:${app.get("port")}/`);
